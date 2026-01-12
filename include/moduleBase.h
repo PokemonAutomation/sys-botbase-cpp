9915f03efbe3c423ed7e9fc5f5ffb9e09002f437
Koi <koi.github3088@gmail.com>
2026-01-12 00:15:22 -0600

    Bump version

--- include/moduleBase.h
+++ include/moduleBase.h
@@ -89,7 +89,7 @@ namespace ModuleBase {
 
 	private:
 		static std::string getCurrentSbbVersion() {
-            return !g_enableBackwardsCompat ? "3.2\r\n" : "3.21\r\n";
+            return !g_enableBackwardsCompat ? "3.3\r\n" : "3.31\r\n";
         }
 
         void setButtonClickSleepTime(const std::vector<std::string>& params);
